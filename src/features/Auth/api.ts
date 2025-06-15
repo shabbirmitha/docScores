3f1c2061c7742c30dc6a1313a7bb63fcee58a825
Shabbir <shabbirmitha786@gmail.com>
2025-06-16 00:06:18 +0530

    fix:url

--- src/features/Auth/api.ts
+++ src/features/Auth/api.ts
@@ -2,5 +2,5 @@ import { api } from "@api/index";
 import { loginFormType } from "./types";
 
 export const login = async (data: loginFormType) => {
-  return await api.post("/login", data);
+  return await api.post("/user/login", data);
 };
